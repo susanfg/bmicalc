b91573d18fac990bd5681c65bc03b51a44c31002
José Miguel Horcas <horcas@uma.es>
2023-02-07 11:54:06 +0100

    Add display name for test

--- src/test/java/bmicalc/BMICalcTest.java
+++ src/test/java/bmicalc/BMICalcTest.java
@@ -1,11 +1,14 @@
 package bmicalc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BMICalcTest {
 	
 	@Test
+	@DisplayName("Example Test Method.")
 	public void bmi() {
 		assertEquals(true, true);
 	}
