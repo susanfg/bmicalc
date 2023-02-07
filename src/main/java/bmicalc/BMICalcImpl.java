96d629ea0d1301fcb51a15301da1eee1a8fbbf39
José Miguel Horcas <horcas@uma.es>
2023-02-07 10:50:44 +0100

    Added empty implementation

--- src/main/java/bmicalc/BMICalcImpl.java
+++ src/main/java/bmicalc/BMICalcImpl.java
@@ -3,21 +3,15 @@ package bmicalc;
 public class BMICalcImpl implements BMICalc {
 
 	public double bmi(double mass, double height) {
-		return mass / (height * height);
+		return 0.0;
 	}
 
 	public String category(double bmi) {
-		String result;
-		if (bmi < 18.5) result = "UNDERWEIGHT";
-		else if (bmi < 25.0) result = "NORMAL";
-		else if (bmi < 30.0) result = "OVERWEIGHT";
-		else result = "OBESE";
-		return result;
+		return "";
 	}
 
 	public boolean abdominalObesity(double waistCircumference, char gender) {
-		return (gender == 'M' && waistCircumference > 90.0) || 
-			   (gender == 'F' && waistCircumference > 80.0);
+		return false;
 	}
 
 }
--- src/test/java/bmicalc/BMICalcTest.java
+++ src/test/java/bmicalc/BMICalcTest.java
@@ -4,18 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class BMICalcTest {
-
-	public static final double DELTA = 0.01;
-	private final BMICalc c = new BMICalcImpl();
 	
 	@Test
 	public void bmi() {
-		assertEquals(28.44, c.bmi(64, 1.5), DELTA);
-		//assertEquals(0, c.bmi(0, 0));
-	}
-	
-	@Test
-	public void bmi2() {
 		assertEquals(true, true);
 	}
 }
