6b21f748e80c65a2438658dd2737774a5177d50d
susanfg <susanafernandez@uma.es>
2023-02-11 19:31:05 +0100

    First tests for BMICalcImpl

--- src/test/java/bmicalc/BMICalcTest.java
+++ src/test/java/bmicalc/BMICalcTest.java
@@ -7,9 +7,36 @@ import org.junit.jupiter.api.Test;
 
 public class BMICalcTest {
 	
+	private BMICalcImpl c = new BMICalcImpl();;
+	
 	@Test
-	@DisplayName("Example Test Method.")
-	public void bmi() {
-		assertEquals(true, true);
+	@DisplayName("Negative Weight")
+	// If the weight is a negative value, it has to return an error message
+	public void bmiNegativeWeight() {
+		assertEquals("Invalid weight", c.bmi(-1, 160));
+	}
+	@Test
+	@DisplayName("Weight = 0")
+	// If the weight is equal to zero, it has to return an error message
+	public void bmiZeroWeight() {
+		assertEquals("Invalid weight", c.bmi(0, 160));
+	}
+	@Test
+	@DisplayName("Negative Height")
+	// If the height is a negative value, it has to return an error message
+	public void bmiNegativeHeight() {
+		assertEquals("Invalid height", c.bmi(70, -1));
+	}
+	@Test
+	@DisplayName("Height = 0")
+	// If the height is equal to zero, it has to return an error message
+	public void bmiZeroHeight() {
+		assertEquals("Invalid height", c.bmi(70, 0));
+	}
+	@Test
+	@DisplayName("Valid values")
+	// If the weight is 100 kg and the height is 190 cm, it has to return 0.002770083102
+	public void bmiBothCorrect() {
+		assertEquals(0.002770083102, c.bmi(-1, 160));
 	}
 }
