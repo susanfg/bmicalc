ef44e5db0bcfec9146573d448702a854a185eef2
susanfg <susanafernandez@uma.es>
2023-02-12 13:18:34 +0100

    correction of the tests and first implementation of the bmi method

--- src/main/java/bmicalc/BMICalcImpl.java
+++ src/main/java/bmicalc/BMICalcImpl.java
@@ -2,8 +2,22 @@ package bmicalc;
 
 public class BMICalcImpl implements BMICalc {
 
+	/**
+	 * Calculate the Body Mass Index (BMI) of a person.
+	 * BMI = mass (kg) / height^2 (m)
+	 * 
+	 * @param mass 	 Weight of the person.
+	 * @param height Height of the person.
+	 * @return 		 The Body Mass Index (BMI) value.
+	 */
 	public double bmi(double mass, double height) {
-		return 1.0;
+		
+		if(mass <= 0 || height <=0) {
+			return  -1;
+		}
+		
+		double bmi = mass / Math.pow(height, 2);
+		return bmi;
 	}
 
 	public String category(double bmi) {
--- src/test/java/bmicalc/BMICalcTest.java
+++ src/test/java/bmicalc/BMICalcTest.java
@@ -11,32 +11,32 @@ public class BMICalcTest {
 	
 	@Test
 	@DisplayName("Negative Weight")
-	// If the weight is a negative value, it has to return an error message
+	// If the weight is a negative value, it has to return -1
 	public void bmiNegativeWeight() {
-		assertEquals("Invalid weight", c.bmi(-1, 160));
+		assertEquals(-1, c.bmi(-1, 160));
 	}
 	@Test
 	@DisplayName("Weight = 0")
-	// If the weight is equal to zero, it has to return an error message
+	// If the weight is equal to zero, it has to return -1
 	public void bmiZeroWeight() {
-		assertEquals("Invalid weight", c.bmi(0, 160));
+		assertEquals(-1, c.bmi(0, 160));
 	}
 	@Test
 	@DisplayName("Negative Height")
-	// If the height is a negative value, it has to return an error message
+	// If the height is a negative value, it has to return -1
 	public void bmiNegativeHeight() {
-		assertEquals("Invalid height", c.bmi(70, -1));
+		assertEquals(-1, c.bmi(70, -1));
 	}
 	@Test
 	@DisplayName("Height = 0")
-	// If the height is equal to zero, it has to return an error message
+	// If the height is equal to zero, it has to return -1
 	public void bmiZeroHeight() {
-		assertEquals("Invalid height", c.bmi(70, 0));
+		assertEquals(-1, c.bmi(70, 0));
 	}
 	@Test
 	@DisplayName("Valid values")
 	// If the weight is 100 kg and the height is 190 cm, it has to return 0.002770083102
 	public void bmiBothCorrect() {
-		assertEquals(0.002770083102, c.bmi(-1, 160));
+		assertEquals(0.0025390625, c.bmi(65, 160));
 	}
 }
